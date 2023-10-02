2b8d36aef02cb037a365a418efdc6b827472b382
andrianang <andrianandrian503@gmail.com>
2023-10-02 12:25:19 +0700

    adding Controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Penerbangan;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    
+    public function index(){
+        $posts = Penerbangan::all(); 
+    
+        return response()->json(['posts' => $posts]);
+    }
+}
